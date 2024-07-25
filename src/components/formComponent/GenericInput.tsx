7960ebb248cc6c90912a2ab344f9e2531a642658
Keshav Kumar <keshavkumar@Keshavs-MacBook-Pro.local>
2024-07-25 10:29:01 +0530

    added control and controller of react form

--- src/components/formComponent/GenericInput.tsx
+++ src/components/formComponent/GenericInput.tsx
@@ -1,29 +1,33 @@
 import { FC, InputHTMLAttributes } from 'react'
-import { useFormContext } from 'react-hook-form'
+import { Controller, useFormContext } from 'react-hook-form'
 const GenericInput: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-    const {
-        register,
-        formState: { errors },
-    } = useFormContext()
+    const { control } = useFormContext()
 
     return (
-        <div className="flex flex-col py-2">
-            <p className=" font-default-font-family text-text-grey text-[0.8rem]">
-                {props.placeholder}
-            </p>
-            <input
-                className=" outline-none font-default-font-family placeholder-[#ABABB2] border-[0.1rem] border-[#C0CAD4] p-[0.8rem] text-[0.9rem] rounded-md"
-                {...register(props.name!)}
-                {...props}
-            />
-            <div className="h-1">
-                {errors[props.name!]?.message && (
-                    <span className="text-[0.8rem] text-red-700 mt-1">
-                        {errors[props.name!]?.message as string}
-                    </span>
-                )}
-            </div>
-        </div>
+        <Controller
+            control={control}
+            name={props.name as string}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <div className="flex flex-col py-2">
+                    <p className=" font-default-font-family text-text-grey text-[0.8rem]">
+                        {props.placeholder}
+                    </p>
+                    <input
+                        className=" outline-none font-default-font-family placeholder-[#ABABB2] border-[0.1rem] border-[#C0CAD4] p-[0.8rem] text-[0.9rem] rounded-md"
+                        onChange={onChange}
+                        value={value}
+                        {...props}
+                    />
+                    <div className="h-1">
+                        {error?.message && (
+                            <span className="text-[0.8rem] text-red-700 mt-1">
+                                {error.message}
+                            </span>
+                        )}
+                    </div>
+                </div>
+            )}
+        />
     )
 }
 
